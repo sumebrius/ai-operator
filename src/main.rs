c6f483c0790e64dd87a09e00e4fa86a066f5a9aa
Bruce Cutler <bruce@sumebrius.net>
2025-11-11 21:35:51 +1300

    Better logging setup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #[macro_use]
-extern crate log;
+extern crate tracing;
 
 use axum::{
     Router,
     routing::{get, post},
 };
+use tower_http::trace::TraceLayer;
 
 use crate::config::AppState;
 
@@ -13,11 +14,15 @@ mod openai;
 
 #[tokio::main]
 async fn main() {
-    config::configure();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .compact()
+        .init();
 
     let webhook_server = Router::new()
         .route("/", get(|| async { "Sup" }))
         .route("/", post(openai::webhook::webhook))
+        .layer(TraceLayer::new_for_http())
         .with_state(AppState::start());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
