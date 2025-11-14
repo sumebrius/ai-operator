13f3c083958f8ecacdee2380d63150ed1a5003c9
Bruce Cutler <bruce@sumebrius.net>
2025-11-14 13:46:40 +1300

    Wrap in Arc for better cloning

--- src/config/state.rs
+++ src/config/state.rs
@@ -5,14 +5,14 @@ use std::{env, fs, sync::Arc};
 #[derive(Clone)]
 pub struct AppState {
     prompt: Bytes,
-    openai_key: String,
+    openai_key: Arc<String>,
     webhook: Option<Arc<Webhook>>,
 }
 
 impl AppState {
     pub fn start() -> Self {
         let prompt = Bytes::from(fs::read("./prompt.txt").expect("No prompt"));
-        let openai_key = env::var("OPENAI_API_KEY").expect("No OpenAI API Key");
+        let openai_key = Arc::new(env::var("OPENAI_API_KEY").expect("No OpenAI API Key"));
         let webhook = env::var("OPENAPI_WEBHOOK_SECRET")
             .ok()
             .map(|secret| Webhook::new(&secret).expect("Bad Webhook Key"))
