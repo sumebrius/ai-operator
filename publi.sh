911989f01e1a50ffe0923c99250047313eb42dfe
Bruce Cutler <bruce@sumebrius.net>
2026-03-21 18:27:05 +1300

    publish script improvement for rebuilds

--- publi.sh
+++ publi.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/bash
 
-set -e
+set -euf pipefail
 
 VERSION=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' --raw-output)
+TAG="v${VERSION}"
 podman login --authfile ~/.config/containers/auth.json
-podman build -t registry.sumebrius.com/library/ai-operator:latest -t registry.sumebrius.com/library/ai-operator:${VERSION} .
+podman build --no-cache -t registry.sumebrius.com/library/ai-operator:latest -t registry.sumebrius.com/library/ai-operator:${VERSION} .
 podman push registry.sumebrius.com/library/ai-operator:latest registry.sumebrius.com/library/ai-operator:${VERSION}
-git tag v${VERSION}
-git push origin v${VERSION}
+
+set +e
+git show-ref --tags ${TAG} --quiet
+TAGGED=$?
+set -e
+if [ $TAGGED -eq 0 ]
+then
+    echo "Tag ${TAG} already exists"
+    exit 0
+fi
+
+git tag ${TAG}
+git push origin ${TAG}
